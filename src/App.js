944a8136fc75d17400002ba552f0475795c97f45
Jacob Price <j@cobprice.com>
2017-07-05 22:15:50 -0400

    Begin ContactList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World
+      <div className="App">
+        <ContactList contacts={[
+          { name: 'Jacob' },
+          { name: 'Jane' },
+          { name: 'Doe' }
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Ainsley' },
+          { name: 'John' },
+          { name: 'Adam' }
+        ]}/>
       </div>
-    )
+    );
   }
 }
 
